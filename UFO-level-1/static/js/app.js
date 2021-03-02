23e655f34d603579898196af1b4355217ee15e2b
Luis Fernando Ruiz <luisfruizlopez@gmail.com>
2021-03-01 23:16:50 -0600

    UFO-Level-1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,50 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+function generateTable(table, data) {
+  var tableHtml = d3.select("tbody")
+  tableHtml.html("")
+  for (let element of data) {
+    // add a row 
+    let row = table.insertRow();
+    for (key in element) {
+      // creates a new cell
+      let cell = row.insertCell();
+      // creates a new text node
+      let text = document.createTextNode(element[key]);
+      // appends the text node to the cell
+      cell.appendChild(text);
+    }
+  }
+};
+// grab table and pass that to our function
+let table = document.querySelector("tbody");
+generateTable(table, tableData);
+
+// --- another way -----
+// tableData.forEach((weatherReport) => {
+//   var row = d3.select("tbody").append("tr");
+//   Object.entries(weatherReport).forEach(([key, value]) => {
+//     var cell = row.append("td");
+//     cell.text(value);
+//   });
+// });
+
+
+// --------------------------------------------------------
+button= d3.select("#filter-btn");
+
+button.on("click",function(){
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  input=d3.select("#datetime");
+  inputValue=input.property("value");
+  filterByDate=tableData.filter(date => date.datetime === inputValue);
+  console.log("filter",filterByDate);
+  generateTable(table,filterByDate);
+
+});
+reset=d3.select("#reset-btn");
+reset.on("click",function(){
+  generateTable(table,tableData);
+});
